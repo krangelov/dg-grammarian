3b50afeff270982fa32989306b43788166523fe9
krangelov <kr.angelov@gmail.com>
2019-06-19 20:27:41 +0200

    working basic functionality for conceptual editing

---
+++ js/dg-conceptual-editor.js
@@ -0,0 +1,205 @@
+function ConceptualEditor(xmlDoc) {
+	this.xmlDoc = xmlDoc;
+
+	var notWhitespace = /\S/
+
+	function filter(root) {
+		var i = 0;
+		while (i < root.childNodes.length) {
+			var node = root.childNodes[i];
+			if ((node.nodeType == 3) && (!notWhitespace.test(node.nodeValue))) {
+				// that is, if it's a whitespace text node
+				root.removeChild(node);
+			} else {
+				filter(node);
+				i++;
+			}
+		}
+	}
+
+	filter(xmlDoc.documentElement);
+}
+ConceptualEditor.prototype.getNode = function(id) {
+	return this.xmlDoc.getElementById(id);
+}
+ConceptualEditor.prototype.getSentences = function(id) {
+	return this.xmlDoc.getElementsByTagName("sentence");
+}
+ConceptualEditor.prototype.getAbstractSyntax = function(node,context) {
+	if (node.nodeName == "sentence") {
+		return this.getAbstractSyntaxSentence(node,context);
+	} else if (node.nodeName == "function") {
+		return this.getAbstractSyntaxFunction(node,context);
+	} else if (node.nodeName == "option") {
+		return this.getAbstractSyntaxOption(node,context);
+	} else if (node.nodeName == "boolean") {
+		return this.getAbstractSyntaxOption(node,context);
+	} else if (node.nodeName == "call") {
+		return this.getAbstractSyntaxCall(node,context);
+	} else if (node.nodeName == "argument") {
+		return this.getAbstractSyntaxArgument(node,context);
+	}
+	return null;
+}
+ConceptualEditor.prototype.getAbstractSyntaxSentence = function(node,context) {
+	var child = node.firstChild;
+	var expr = this.getAbstractSyntax(child,context);
+	context.trim();
+	return expr;
+}
+ConceptualEditor.prototype.getAbstractSyntaxFunction = function(node,context) {
+	var func      = node.getAttribute("name");
+	var arguments = node.childNodes;
+	if (arguments == null || arguments.length == 0) {
+		// context.incrementFId(function.size()); --??
+		if (func.indexOf(' ') >= 0)
+			func = "("+func+")";
+		return func;
+	}
+
+	// context.incrementFId(function.size()-1); --??
+
+	var expr = func;
+	for (var i = 0; i < arguments.length; i++) {
+		expr += " " + this.getAbstractSyntax(arguments[i],context);
+	}
+	expr = "("+expr+")";
+
+	// context.incrementFId(1);
+
+	return expr;
+}
+ConceptualEditor.prototype.getAbstractSyntaxOption = function(node,context) {
+	var options = node.childNodes;
+	var parent  = context.changeParentNode(node);
+	var expr = this.getAbstractSyntax(options[context.choose(node)],context);
+	context.changeParentNode(parent);
+	return expr;
+}
+ConceptualEditor.prototype.getAbstractSyntaxCall = function(node,context) {
+	var ref       = this.xmlDoc.getElementById(node.getAttribute("ref"));
+	var arguments = node.childNodes;
+
+	context.push(arguments);
+	var res = this.getAbstractSyntax(ref,context);
+	context.pop();
+	return res;
+}
+ConceptualEditor.prototype.getAbstractSyntaxArgument = function(node,context) {
+	var attr = node.getAttribute("index");
+	var index = (attr == null) ? 0 : parseInt(attr);
+	return this.getAbstractSyntax(context.getArgument(index),context);
+}
+
+function ChoiceContext() {
+	this.pos     = 0;
+	this.choices = [];
+	this.stack   = [];
+	this.parentNode = null;
+	this.fids    = [];
+}
+ChoiceContext.prototype.choose = function(node) {
+	return this.getNodeChoice(node).getChoice();
+}
+ChoiceContext.prototype.getNodeChoice = function(node) {
+	var choice = null;
+
+	function unlink(node) {
+		for (;;) {
+			var ref = node.getAttribute("ref");
+			if (ref == null)
+				return node;
+		}
+	}
+
+	if (this.pos < this.choices.length) {
+		if (unlink(this.choices[this.pos].getNode()) == unlink(node)) {
+			choice = this.choices[this.pos];
+		} else {
+			this.trim();
+		}
+	}
+
+	if (choice == null) {
+		choice = new SyntacticChoice(node, this);
+		this.choices.push(choice);
+	}
+
+	this.pos++;
+	return choice;
+}
+ChoiceContext.prototype.getDefaultChoice = function(node) {
+	if (node.nodeName != "option")
+		return 0;
+
+	var default_id     = node.getAttribute("default");
+	var persistence_id = node.getAttribute("persistence_id");
+	var options        = node.childNodes;
+
+	var position = 0;
+	if (default_id != null) {
+		for (var i = 0; i < options.length; i++) {
+			if (default_id == options[i].id) {
+				position = i;
+				break;
+			}
+		}
+	}
+
+	if (persistence_id != null) {
+		position = this.getInt(persistence_id, position);
+	}
+
+	return position;
+}
+ChoiceContext.prototype.getDefaultLiteral = function(node) {
+	if (node.nodeName != "string")
+		return null;
+
+	var default_value  = node.getAttribute("default");
+	var persistence_id = node.getAttribute("persistence_id");
+
+	if (persistence_id != null) {
+		default_value = this.getString(persistence_id, default_value);
+	}
+	return "\""+default_value+"\"";
+}
+ChoiceContext.prototype.push = function(args) {
+	this.stack.push(args);
+}
+ChoiceContext.prototype.pop = function() {
+	this.stack.pop();
+}
+ChoiceContext.prototype.getArgument = function(i) {
+	return this.stack[this.stack.length-1][i];
+}
+ChoiceContext.prototype.reset = function() {
+	this.pos = 0;
+	this.stack = [];
+	this.parentNode = null;
+	this.fids = [];
+}
+ChoiceContext.prototype.trim = function() {
+	while (this.pos < this.choices.length)
+		this.choices.pop();
+}
+ChoiceContext.prototype.changeParentNode = function(node) {
+}
+
+function SyntacticChoice(node,context) {
+	this.node   = node;
+	this.choice = context.getDefaultChoice(node);
+	this.literal= context.getDefaultLiteral(node);
+}
+SyntacticChoice.prototype.getChoice = function() {
+	return this.choice;
+}
+SyntacticChoice.prototype.setChoice = function(i) {
+	this.choice = i;
+}
+SyntacticChoice.prototype.getNode = function() {
+	return this.node;
+}
+SyntacticChoice.prototype.getOptions = function() {
+	return this.node.childNodes;
+}
--- js/dg-grammarian.js
+++ js/dg-grammarian.js
@@ -1,58 +1,77 @@
-dg_grammarian = {}
+dg_grammarian = {
+	context: null,
+	xmlNode: null
+}
 
-dg_grammarian.parse = function (sentence, result) {
-	function errcont(text,code) { }
+dg_grammarian.grammar_url = "http://cloud.grammaticalframework.org/grammars/Phrasebook.pgf"
+
+dg_grammarian.grammar_call=function(querystring,cont,errcont) {
+    http_get_json(this.grammar_url+querystring,cont,errcont)
+}
+
+dg_grammarian.errcont = function(text,code) { }
+
+dg_grammarian.parse = function (selection, sentence, linearization, choices) {
 	function extract_linearization(lins) {
-		function taggedBrackets(brackets) {
-			var tags = [];
-			for (var i in brackets) {
-				if ("bind" in brackets[i])
-					bind_state = brackets[i].bind;
-				else {
-					if (!bind_state) {
-						tags.push(text(" "));
-						bind_state = true;
-					}
-
-					if ("token" in brackets[i]) {
-						tags.push(text(brackets[i].token));
-						bind_state = false;
-					} else {
-						tags.push(node("span", {"fid": brackets[i].fid,
-												"fun": brackets[i].fun,
-												"onclick": "gfwordnet.onclick_bracket(event, this)"},
-									   taggedBrackets(brackets[i].children)));
-					}
-				}
-			}
-			return tags;
-		}
-		var rows = []
-		for (var i in lins) {
-			var lin = lins[i];
-			bind_state = true;
-			rows.push(tr([td(taggedBrackets(lin.brackets))]));
-		}
-		result.appendChild(node("table",{class: "result"},rows), this.nextSibling);
+		dg_grammarian.update_linearization(selection, lins, linearization, choices);
+		choices.innerHTML="";
 	}
 	function extract_parse(parses) {
-		gfwordnet.grammar_call("?command=c-bracketedLinearize&to=ParseEng&tree="+encodeURIComponent(parses[0].trees[0].tree),extract_linearization,errcont);
+		if ("trees" in parses[0]) {
+			dg_grammarian.grammar_call("?command=c-bracketedLinearize&to="+selection.langs_list.join("%20")+"&tree="+encodeURIComponent(parses[0].trees[0].tree),extract_linearization,dg_grammarian.errcont);
+		} else {
+			linearization.innerHTML="";
+			choices.innerHTML="";
+		}
 	}
-	gfwordnet.grammar_call("?command=c-parse&from=ParseEng&input="+encodeURIComponent(sentence),extract_parse,errcont);
+	dg_grammarian.grammar_call("?command=c-parse&from=PhrasebookEng&input="+encodeURIComponent(sentence),extract_parse,dg_grammarian.errcont);
+}
+dg_grammarian.update_linearization = function (selection, lins, linearization, choices) {
+	function taggedBrackets(brackets) {
+		var tags = [];
+		for (var i in brackets) {
+			if ("bind" in brackets[i])
+				bind_state = brackets[i].bind;
+			else {
+				if (!bind_state) {
+					tags.push(text(" "));
+					bind_state = true;
+				}
+
+				if ("token" in brackets[i]) {
+					tags.push(text(brackets[i].token));
+					bind_state = false;
+				} else {
+					tags.push(node("span", {"fid": brackets[i].fid,
+											"fun": brackets[i].fun,
+											"onclick": "dg_grammarian.onclick_bracket(event, this)"},
+								   taggedBrackets(brackets[i].children)));
+				}
+			}
+		}
+		return tags;
+	}
+	var rows = []
+	for (var i in lins) {
+		var lin = lins[i];
+		bind_state = true;
+		rows.push(tr([th(text(selection.langs[lin.to].name)),td(taggedBrackets(lin.brackets))]));
+	}
+	linearization.innerHTML="";
+	linearization.appendChild(node("table",{class: "result"},rows), this.nextSibling);
 }
 dg_grammarian.load_phrases = function(url) {
 	function extract_phrases(xml) {
-		dg_grammarian.xmlDoc = xml.responseXML;
+		dg_grammarian.editor =
+			new ConceptualEditor(xml.responseXML);
 
 		var table = document.getElementById("phrases");
-		var nodes = dg_grammarian.xmlDoc.documentElement.childNodes;
+		var nodes = dg_grammarian.editor.getSentences();
 		for (var i = 0; i < nodes.length; i++) {
-			if (nodes[i].nodeName == "sentence") {
-				table.appendChild(tr(node("td", {onclick: "dg_grammarian.onclick_sentence('"+nodes[i].getAttribute("id")+"')"}, [text(nodes[i].getAttribute("desc"))])));
-			}
+			table.appendChild(tr(node("td", {onclick: "dg_grammarian.onclick_sentence(getMultiSelection(element('from')), '"+nodes[i].getAttribute("id")+"',element('linearization'), element('choices'))"}, [text(nodes[i].getAttribute("desc"))])));
 		}
 	}
-		
+
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
@@ -62,7 +81,37 @@ dg_grammarian.load_phrases = function(url) {
 	xhttp.open("GET", url, true);
 	xhttp.send();
 }
-dg_grammarian.onclick_sentence = function(id) {
-	var node = dg_grammarian.xmlDoc.getElementById(id);
-	alert(node);
+dg_grammarian.regenerate = function(selection,linearization,choices) {
+	var expr = this.editor.getAbstractSyntax(this.xmlNode,this.context);
+
+	function extract_linearization(lins) {		
+		dg_grammarian.update_linearization(selection, lins, linearization, choices);
+	}
+	dg_grammarian.grammar_call("?command=c-bracketedLinearize&to="+selection.langs_list.join("%20")+"&tree="+encodeURIComponent(expr),extract_linearization,dg_grammarian.errcont);
+
+	choices.innerHTML = "";
+	for (var i in this.context.choices) {
+		var choice = this.context.choices[i];
+		var edit   = node("select", {onchange: "dg_grammarian.onchange_option("+i+",this.value,getMultiSelection(element('from')), element('linearization'), element('choices'))"}, []);
+
+		var nodes = choice.getOptions();
+		for (var j = 0; j < nodes.length; j++) {
+			var option = node("option", {value: j}, [text(nodes[j].getAttribute("desc"))]);
+			if (j == choice.getChoice())
+				option.selected = true;
+			edit.appendChild(option);
+		}
+
+		choices.appendChild(tr(td(edit)));
+	}
+}
+dg_grammarian.onclick_sentence = function(selection,id,linearization,choices) {
+	this.context = new ChoiceContext();
+	this.xmlNode = this.editor.getNode(id);
+	this.regenerate(selection,linearization,choices);
+}
+dg_grammarian.onchange_option = function(i,j,selection,linearization,choices) {
+	this.context.choices[i].setChoice(j);
+	this.context.reset();
+	this.regenerate(selection,linearization,choices);
 }
